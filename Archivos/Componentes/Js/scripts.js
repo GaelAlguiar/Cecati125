94c1e9f90efeccd4daec32d2b37b80372b037ec7
Gael Alguiar <gaelalguiar@gmail.com>
2023-04-27 21:31:54 -0600

    Agregue apartado de noticias

--- Archivos/Componentes/Js/scripts.js
+++ Archivos/Componentes/Js/scripts.js
@@ -1,7 +1,7 @@
-//Create function to select elements
 const selectElement = (element) => document.querySelector(element);
-//Open and close nav on click
 
 selectElement('.icons').addEventListener('click', () => {
         selectElement('nav').classList.toggle('active');
 });
+
+
